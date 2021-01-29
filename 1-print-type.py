54f0495837b4cbeec85b179cd5c8f42c9dfab842
Ali Sezişli <ali@alisezisli.com.tr>
2021-01-29 16:43:32 +0300

    1-print-type.py eklendi
    String değerlerden bahsedildi. print ve type fonksiyonları örneklendirildi.

---
+++ 1-print-type.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+
+#Python dilinde yorum satırları '#' karakteriyle başlar.
+#Bu gördüğünüz satırlar yorum satırlarıdır ve Python yorumlayıcısı tarafından göz ardı edilir.
+
+
+#En üstte gördüğünüz satır ise "shebang" satırıdır.
+#Shell'imize, bu dosyanın /usr/bin/python3 programı aracılığıyla çalıştırılacağını bildirir.
+#Shebang satırı, belgenin en üstünde bulunmalıdır ve her zaman '#!' ile başlamalıdır.
+
+
+#Python dilinde çift tırnak işaretleri arasında göreceğiniz değerler, "string" tipinde değerlerdir.
+#String değerler, karakterleri tutar: Harf, rakam, sembol, boşluk, tab, yeni satır gibi.
+
+
+#Aşağıda bazı string örnekleri verilmiştir:
+"Hayko Cepkin"
+"Haggard"
+"Gazapizm"
+"Ragga Oktay"
+
+
+#Python'da ekrana herhangi bir şey yazdırmak için "print" fonksiyonunu kullanırız:
+print("Hayko Cepkin")
+print("Bucaspor")
+
+
+
+
+#Python'da bir değerin  tipini öğrenmek için "type" fonksiyonunu kullanırız:
+type("Hayko Cepkin")
+type("Bucaspor")
+
+
+#Yukarıdaki fonksiyonun çıktısını görebilmek için, print'ten faydalanabiliriz:
+print(type("Hayko Cepkin"))
+print(type("Bucaspor"))
+#Ekrana "<class 'str'> yazar
