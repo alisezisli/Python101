915d0c237a1354da32010de4283ad458e34d8be8
Ali Sezişli <ali@alisezisli.com.tr>
2021-01-29 17:08:39 +0300

    2-aritmetik-islecler.py eklendi.
    Aritmetik işleçlerle ilgili örnekler eklendi.

---
+++ 2-aritmetik-islecler.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+
+
+#Python'da aritmetik işleçlerin davranışları hakkında örnekler:
+
+
+#Sayı örnekleri (integer, float):
+
+#Toplama işlemi (+):
+print(2+2)
+
+#Çıkarma işlemi (-):
+print(99-73)
+
+#Çarpma işlemi (*):
+print(32*8)
+
+#Bölme işlemi (/):
+print(22/7)
+
+#Tam (kalansız) bölme işlemi (//):
+print(22//7)
+
+#Kalansız bölme işlemi yukarıya yuvarlamaz!:
+print(7/4)
+print(7//4)
+
+
+
+#Metin örnekleri (string):
+#String'lerde toplama (ekleme, concatenate):
+print("Buca"+"spor")
+
+#Boşluklar ile ekleme:
+print("Tupac"+" "+"Amaru"+" "+"Shakur")
+
+#veya ("Amaru" kelimesinin etrafındaki boşluklara dikkat):
+print("Tupac"+" Amaru "+"Shakur")
+
+#String'lerde çarpma?!?:
+print("Hello world!"*2)
+
+print("="*20)
+
+print("-*-"*10)
+
+print("du hast mich gefragt, "*3 + "und ich hab' nichts gesagt.")
+
